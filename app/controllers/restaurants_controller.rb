d38d213afa089be05a6cdd407441ef18a8b48a1d
Karen Banci <karenbanci1@gmail.com>
2021-11-17 16:56:33 -0300

    preciso continuar

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,26 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "restaurants#index"
+
+  # shallow nao pega o ID do restaurant para destroy
+  resources :restaurants, shallow: true do
+    collection do
+      get :top
+    end
+    member do
+      get :chef
+    end
+    # Resources eh sintaxe do ruby para criar um CRUD
+    resources :reviews, only: %i[new create destroy]
+  end
 end
